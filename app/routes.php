40716cbdde152087ac64b8f05d03dddac0b99b7f
Arnar Geirsson <arnarinn105@gmail.com>
2015-04-25 22:27:32 +0000

    added views/foundation/register

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,6 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('/','HomeController@index');
+Route::get('/register', 'UserController@register');
+Route::post('/register','UserController@post_register');
---
+++ app/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+	<h1>Facewhore</h1>
+@stop
